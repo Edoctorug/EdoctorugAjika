c75e4523862e2586c07371e1dd584521ea4d652f
Karangwa ajika <karangwaajika@gmail.com>
2023-01-24 14:09:22 +0530

    some doing up

--- products/static/products/js/cart.js
+++ products/static/products/js/cart.js
@@ -31,3 +31,23 @@ function updateCart(id, action) {
     .then(response => response.json())
     .then(data => console.log(data))
 }
+
+
+let user = "{{ request.user }}";
+function getCookie(name) {
+let cookieValue = null;
+if (document.cookie && document.cookie !== '') {
+  const cookies = document.cookie.split(';');
+  for (let i = 0; i < cookies.length; i++) {
+      const cookie = cookies[i].trim();
+      // Does this cookie string begin with the name we want?
+      if (cookie.substring(0, name.length + 1) === (name + '=')) {
+          cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+          break;
+      }
+  }
+}
+return cookieValue;
+}
+const csrftoken = getCookie('csrftoken');
+
